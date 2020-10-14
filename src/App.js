9f93c94d3a87457a1768b8f824ecf3d44dbfb5ff
Oluwatomisin <malcolmtomisin@gmail.com>
2020-10-14 20:20:44 +0100

    read from json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
+import React, {
+  useEffect, useState
+} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as json from './counter.json';
 
 function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    console.log('start');
+    let startTime = new Date(json.dateStart).getTime();
+    let endTime = new Date(json.dateEnd).getTime();
+    
+    let timeDiff = endTime - startTime;
+    let unitTime = Math.round(timeDiff / 12);
+    console.log(endTime, unitTime);
+    let timeIntervals = [];
+    for (let i = 0; i < 12; i++) {
+      let intervals = i * unitTime;
+      //console.log(intervals);
+      timeIntervals.push(startTime + intervals);
+    }
+    
+    //console.log(timeIntervals);
+    timeIntervals.push(endTime);
+    let reverseIntervals = timeIntervals.reverse();
+    let result = new Array(timeIntervals.length).fill([]);
+    const { counterHistory } = json;
+    //console.log('...', typeof json.counterHistory);
+    if (json.counterHistory != null) {
+      // reverseIntervals.map((v, i, arr) => {
+      //   json.counterHistory.map((val, index, a) => {
+      //     let millTimeStamp = new Date(val.timestamp).getTime();
+      //     if (v < millTimeStamp) {
+      //       result[i].push(val);
+      //       a.shift();
+      //     }
+      //     })
+      //   })
+      let arrayOfTimeStamps = json.counterHistory.map(v => new Date(v.timestamp).getTime());
+      for (let i = 0; i < reverseIntervals.length; i++){
+        for (let j = 0; j < arrayOfTimeStamps.length; j++){
+          if (arrayOfTimeStamps[j] > reverseIntervals[i]) {
+            result[i].push(arrayOfTimeStamps.splice(j,1));
+            break;
+          }
+        }
+      }
+      console.log(json.counterHistory.length);
+    }
+    for (let i = 0; i < result.length; i++) {
+      console.log(i, result[i].length);
+    }
+    // console.log(result[0][0]);
+    // console.log(result[1][0])
+    // console.log('res', result);
+    // setData(result);
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <div>{`${data}`}</div> */}
     </div>
   );
 }
