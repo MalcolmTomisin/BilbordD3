8c3e6014936b9c7c11a24ad30aeb1a385c471dba
Oluwatomisin <malcolmtomisin@gmail.com>
2020-10-15 15:48:15 +0100

    implement data holder with map

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, {
-  useEffect, useState
+  useEffect, useState, useRef
 } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import * as json from './counter.json';
+import * as d3 from 'd3';
 
 function App() {
 
   const [data, setData] = useState([]);
+  const container = useRef(null);
 
   useEffect(() => {
-    console.log('start');
+    //start and end epochs of efiiwe processing
+    //video files are usually sped and compressed therefore ratio equivalents of real
+    //world recordings are made, then divided into 12 intervals representing 12 hours
+    //from 7am - 7pm
+    
     let startTime = new Date(json.dateStart).getTime();
     let endTime = new Date(json.dateEnd).getTime();
     
+    //
     let timeDiff = endTime - startTime;
     let unitTime = Math.round(timeDiff / 12);
     console.log(endTime, unitTime);
@@ -24,40 +31,27 @@ function App() {
       timeIntervals.push(startTime + intervals);
     }
     
-    //console.log(timeIntervals);
     timeIntervals.push(endTime);
     let reverseIntervals = timeIntervals.reverse();
-    let result = new Array(timeIntervals.length).fill([]);
     const { counterHistory } = json;
-    //console.log('...', typeof json.counterHistory);
+    let result = new Map();
     if (json.counterHistory != null) {
-      // reverseIntervals.map((v, i, arr) => {
-      //   json.counterHistory.map((val, index, a) => {
-      //     let millTimeStamp = new Date(val.timestamp).getTime();
-      //     if (v < millTimeStamp) {
-      //       result[i].push(val);
-      //       a.shift();
-      //     }
-      //     })
-      //   })
       let arrayOfTimeStamps = json.counterHistory.map(v => new Date(v.timestamp).getTime());
-      for (let i = 0; i < reverseIntervals.length; i++){
+      for (let i = startTime; i < endTime; i = i + unitTime){
         for (let j = 0; j < arrayOfTimeStamps.length; j++){
-          if (arrayOfTimeStamps[j] > reverseIntervals[i]) {
-            result[i].push(arrayOfTimeStamps.splice(j,1));
-            break;
-          }
+          if (arrayOfTimeStamps[j] > i && arrayOfTimeStamps[j] < i+unitTime) {
+            //console.log(i, 'here');
+            if (result.has(i)) {
+              result.set(i, [...result.get(i), json.counterHistory[j]]);
+            }
+            else {
+              result.set(i, [json.counterHistory[j]]);
+            }
+            }
         }
       }
-      console.log(json.counterHistory.length);
+      //setData(Array.from(result));
     }
-    for (let i = 0; i < result.length; i++) {
-      console.log(i, result[i].length);
-    }
-    // console.log(result[0][0]);
-    // console.log(result[1][0])
-    // console.log('res', result);
-    // setData(result);
   },[])
   return (
     <div className="App">
